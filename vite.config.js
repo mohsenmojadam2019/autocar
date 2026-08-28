f713a35a0037defa2d2a97cc2904cc1897f14ac2
Mohsen Mojaddam محسن مجدم <56688766+mohsenmojadam2019@users.noreply.github.com>
2026-08-28 16:27:31 +0330

    build(AD-021): bundle modern admin theme

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
             'resources/css/extensions.css',
             'resources/css/ux.css',
             'resources/css/autocar-theme.css',
+            'resources/css/autocar-admin.css',
             'resources/js/vendor.js',
             'resources/js/app.js',
             'resources/js/ux.js',
